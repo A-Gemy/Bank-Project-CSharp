b003e84c9298bea32de9709b15f7a9c0b0908f9c
Ahmed Gamal <ahmgy.mail@gmail.com>
2026-01-15 05:01:24 +0200

    Start with basic clsPerson class

---
+++ Core/clsPerson.cs
@@ -0,0 +1,31 @@
+﻿namespace Bank_Project_CSharp.Core
+{
+    internal class clsPerson
+    {
+        private string _FirstName { get; set; }
+        private string _LastName { get; set; }
+        private string _Email { get; set; }
+        private string _Phone { get; set; }
+
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public string FullName
+        {
+            get => _FirstName + " " + _LastName;
+        }
+
+
+        clsPerson(string firstName, string lastName, string email, string phone)
+        {
+            _FirstName = firstName;
+            _LastName = lastName;
+            _Email = email;
+            _Phone = phone;
+        }
+
+    }
+}
